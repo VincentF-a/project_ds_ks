c61761d6bbdb64d582502b3431151800e38c86c8
Vincent Fan <fanvincent2@gmail.com>
2022-05-25 11:43:17 +0900

    Added year selection

--- data_process/data_process.py
+++ data_process/data_process.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+from datetime import datetime as dt
 
 df_migration = pd.read_csv('data/dataset_past_migrations.csv')
 
@@ -10,4 +11,4 @@ list_cities = [city for city in list_cities_out if city in list_cities_in]
 
 raw_list_quarters = df_migration.columns.values.tolist()
 list_quarters = [quarter for quarter in raw_list_quarters if re.match(r'[1-2][0-9][0-9][0-9]',quarter)]
-
+list_years = list(set([int(quarter[:4]) for quarter in list_quarters]))
--- main.py
+++ main.py
@@ -7,16 +7,14 @@ from data_process import data_process as dp
 st.sidebar.title('Welcome to our project')
 
 st.sidebar.markdown('This project is aimed at displaying the **demographic evolution** inside South Korea and predict its future evolutions.')
-st.sidebar.multiselect('Select the cities',dp.list_cities)
+st.sidebar.multiselect('Select the cities',dp.list_cities,default=dp.list_cities)
 
-st.sidebar.slider('Select a time frame', min_value=dp.list_quarters[0], max_value=dp.list_quarters[-1], value=dp.list_quarters)
+start_year_col, end_year_col = st.sidebar.columns(2)
+with start_year_col:
+    start_year = st.selectbox('Select the start year', options=dp.list_years)
+with end_year_col:
+    updated_years = dp.list_years[dp.list_years.index(start_year):]
+    end_year = st.selectbox('Select the end year', options=updated_years, index=len(updated_years)-1)
 
-st.dataframe(dp.df_migration)
-st.markdown(dp.df_migration.shape)
-
-st.markdown(dp.list_cities_in)
-st.markdown(dp.list_cities_out)
-st.markdown(dp.list_cities)
-
-st.markdown(dp.raw_list_quarters)
-st.markdown(dp.list_quarters)
+if st.sidebar.button('Visualize the results'):
+    st.markdown('todo')
